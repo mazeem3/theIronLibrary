4f7c0926851884c11747eb8558bb44c32ac8d7df
mazeem3 <maaz.azeem3@yahoo.com>
2016-06-30 21:47:05 -0500

    another attempt for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@ Author.delete_all
 Book.delete_all
 
 Author.create first_name: "Maaz", last_name: "Azeem", bio: "A horrible author"
+Book.create title: "Test", photo_url:"http://pngimg.com/upload/book_PNG2116.png", price: 8.99, author_id: 1
