91f07f14a12d308e0e258857eae93090381cb011
mazeem3 <maaz.azeem3@yahoo.com>
2016-07-01 22:45:14 -0500

    Fixed validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,9 @@
 class Author < ActiveRecord::Base
     has_many :books
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :bio, presence: true
+
 
     def full_name
       [first_name, last_name].join(' ')
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ActiveRecord::Base
     belongs_to :author
+    validates :title, presence: true
+    validates :photo_url, presence: true
+    validates :price, presence: true
 end
