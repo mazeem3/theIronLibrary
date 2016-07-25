78b05b9f31bdceb353dea3a15cf17fb50d5a4e73
mazeem3 <maaz.azeem3@yahoo.com>
2016-07-25 12:28:57 -0500

    added faker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
 
 Author.delete_all
 Book.delete_all
@@ -13,3 +7,9 @@ User.delete_all
 maaz = User.create! username: "maaz", password: "12345678"
 author  = Author.create! first_name: "Maaz", last_name: "Azeem", bio: "A horrible author"
 Book.create! title: "Test", photo_url:"http://pngimg.com/upload/book_PNG2116.png", price: 8.99, author_id: author.id
+
+10.times do
+  User.create username: "#{Faker::Internet.user_name}", password: "12345678"
+  i = Author.create! first_name: "#{Faker::Name.first_name}" , last_name: "#{Faker::Name.first_name}", bio: "#{Faker::Lorem.paragraph}"
+  Book.create! title: "#{Faker::Book.title}", photo_url:"http://pngimg.com/upload/book_PNG2116.png", price:"#{Faker::Commerce.price}", author_id: i.id
+end
